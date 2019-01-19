97881be34a5e8f12d7f902972a77ac26a18900d9
oprisan <aoprisan@adobe.com>
2019-01-19 20:37:19 +0200

    making some progress

--- src/main/java/com/adobe/transformer/MainApp.java
+++ src/main/java/com/adobe/transformer/MainApp.java
@@ -1,8 +1,10 @@
 package com.adobe.transformer;
 
+import org.apache.poi.ooxml.POIXMLProperties;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFShape;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
-import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
+import org.apache.poi.xslf.usermodel.XSLFTextShape;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,23 +15,24 @@ public final class MainApp {
         final var pptSample = MainApp.class.getResourceAsStream("/ppt-sample.pptx");
         final var slideShow = new XMLSlideShow(pptSample);
 
-        final var slides = slideShow.getSlides();
-
-        for (final var slide : slides) {
-            if (slide.getNotes() != null) {
-                printContent(slide);
-            } else {
-                System.out.println("Nothing found in slide: " + slide.getTitle());
-            }
-        }
+        printContent(slideShow);
     }
 
-    private static void printContent(XSLFSlide slide) {
-        final List<List<XSLFTextParagraph>> textParagraphs = slide.getNotes().getTextParagraphs();
+    private static void printContent(XMLSlideShow powerPoint) {
+        final POIXMLProperties.CoreProperties props = powerPoint.getProperties().getCoreProperties();
+        final String title = props.getTitle();
+        System.out.println("Title: " + title);
 
-        for (final var listTexParagraph : textParagraphs) {
-            for (final var textParagraph : listTexParagraph) {
-                System.out.println(textParagraph.getText());
+        for (XSLFSlide slide : powerPoint.getSlides()) {
+            System.out.println("Starting slide...");
+            final List<XSLFShape> shapes = slide.getShapes();
+
+            for (final var shape : shapes) {
+                if (shape instanceof XSLFTextShape) {
+                    final XSLFTextShape textShape = (XSLFTextShape) shape;
+                    final String text = textShape.getText();
+                    System.out.println("Text: " + text);
+                }
             }
         }
     }
