38d00353dcec970e748764ccadbebe66771268f4
oprisan <aoprisan@adobe.com>
2019-01-19 20:16:39 +0200

    Working with API

--- src/main/java/com/adobe/transformer/MainApp.java
+++ src/main/java/com/adobe/transformer/MainApp.java
@@ -2,9 +2,18 @@ package com.adobe.transformer;
 
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 
+import java.io.IOException;
+
 public final class MainApp {
 
-    public static void main(String[] args) {
-        final XMLSlideShow xmlSlideShow = new XMLSlideShow();
+    public static void main(String[] args) throws IOException {
+        final var pptSample = MainApp.class.getResourceAsStream("/ppt-sample.pptx");
+        final var slideShow = new XMLSlideShow(pptSample);
+
+        final var slides = slideShow.getSlides();
+
+        for (final var slide : slides) {
+            System.out.println(slide.getSlideName());
+        }
     }
 }
